a4d1248ba83c922aa21447a71246fb68b6650461
Chishamiso Chivhenge <chishachivhenge@yahoo.co.uk>
2020-09-29 11:13:24 +0100

    Modularised Star Match

---
+++ starmatch/ClientApp/src/components/StarMatch.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import { useState } from 'react';
+import Game from "./Game";
+
+import '../CSS/star-match.css'
+
+const StarMatch = () => {
+    const [gameId, setGameId] = useState(1);
+    return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)} />;
+}
+
+
+export default StarMatch;
